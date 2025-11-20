2516baf572c8b7cb3310161f90146c44ebde6550
krishap-7 <krisapkarki11@gmail.com>
2025-11-20 12:08:34 -0800

    Update gui_final_project.py

--- gui_final_project.py
+++ gui_final_project.py
@@ -1,8 +1,13 @@
 from tkinter import *
-
 import tkinter.messagebox as mb
+import tkinter.ttk as tk
+import sqlite3
 
-import tkinter.ttk as ttk
+class Database:
+    def __init__(self,path):
+        self.path = path
+        self.create_table()
+
 
 from gui_final_project_create_db import DB
 
@@ -19,5 +24,6 @@ root.geometry("800x600+351+174")
 
 #create a label widget for the title 
 title_label = Label(root, text="Final Project GUI Application", font=("Helvetica", 16))
+
 
 #declare the functions that will be called in the commands of the different buttons
